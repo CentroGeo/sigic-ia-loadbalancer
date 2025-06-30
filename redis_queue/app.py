6406f47e548ca085e7eaf1ca48a2b8ead5f2213c
Sergio <sage_sergio@hotmail.com>
2025-06-30 12:21:49 -0600

    Agregado la petición de llm en client recetor backend

--- redis_queue/app.py
+++ redis_queue/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from rq import Queue, Worker, Retry
 from rq.job import Job
 from redis import Redis
@@ -7,6 +7,7 @@ from multiprocessing import Process
 import os
 import socket
 import requests
+import time
 
 r = Redis(host='10.2.13.44', port=6379)
 q = Queue(connection=r)
@@ -20,8 +21,33 @@ def start():
     
     return jsonify({"job_id": job.id})
 
+
 @app.route("/process/<job_id>", methods=["GET"])
 def health(job_id):
+    def event_stream():
+        while True:
+            try:
+                job = Job.fetch(job_id, connection=r)
+                
+                status = job.get_status()
+                result = job.result
+                
+                yield f"data: {status}\n\n result: {result}\n\n"
+                
+                if status in ["finished", "failed"]:
+                    break
+
+                time.sleep(10)
+            except Exception as e:
+                yield f"data: {e}\n\n"
+                break
+
+            
+    return Response(event_stream(), mimetype="text/event-stream")
+
+
+@app.route("/process/v1/<job_id>", methods=["GET"])
+def healthV1(job_id):
     job = Job.fetch(job_id, connection=r)
     
     return jsonify ({
@@ -30,7 +56,7 @@ def health(job_id):
         "status": job.get_status(),
         "result": job.result
     })
-
+    
 def start_worker():
     worker = Worker([q], connection=r)
     worker.work(with_scheduler=True)
--- redis_queue/task.py
+++ redis_queue/task.py
@@ -1,14 +1,41 @@
 
 import time 
 import random
+import requests
+import json
 
 def background_task(data):
-    random_boolean = random.choice([True, False, False, False])
-    print("Procesando en segundo plano:", type(data), random_boolean)
-    time.sleep(20)
+    print("Procesando en segundo plano:", type(data))
+    url = "http://10.2.13.44:8080/v1/chat/completions"
     
+    init_date = time.time()
+    respuesta = requests.post(
+        url,
+        headers={"Content-type": "application/json"},
+        data=data,
+        #stream=True
+    )
     
-    if(random_boolean):
-        return data.decode('utf-8')
+    if respuesta.status_code == 200:
+        print("Solicitud exitosa")
+        try:
+            # Si la respuesta es JSON
+            end_date = time.time()
+            data = respuesta.json()
+            print("\n\n**** Respuesta JSON:\n", json.dumps(data, indent=2))
+            delta_date = end_date - init_date
+            print("tiempo (segundos):", delta_date)
+            
+            respuesta_chatbot = data["choices"][0]["message"]["content"]
+            #print("\n\n**** Respuesta modelo: \n",respuesta_chatbot)
+            return respuesta_chatbot                
+        
+        except json.JSONDecodeError:
+            # Si la respuesta no es JSON, manejar como texto
+            print("\n\n*** Respuesta en texto:\n", respuesta.text)
+            return respuesta.text
     else:
+        print(f"Error en la solicitud: {respuesta.status_code}")
+        print(respuesta.text)
         raise Exception("Error intencional para probar retry")
+        
