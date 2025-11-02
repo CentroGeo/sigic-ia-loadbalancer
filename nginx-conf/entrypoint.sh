43dd8330e32130a9ba858471be85c797fac7565d
Niji <mathereall@gmail.com>
2025-11-02 16:23:31 -0600

    fix: update entrypoint script to auto-detect DNS and use nginx.conf template

--- nginx-conf/entrypoint.sh
+++ nginx-conf/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-# Detecta DNS automáticamente
-if [ -z "$NGINX_RESOLVER" ]; then
-  export NGINX_RESOLVER=$(awk '/^nameserver/{print $2; exit}' /etc/resolv.conf)
-  echo "[entrypoint] Resolver auto-detectado: $NGINX_RESOLVER"
-fi
+RESOLVER=$(awk '/^nameserver/{print $2; exit}' /etc/resolv.conf)
+export NGINX_RESOLVER=$RESOLVER
+echo "[entrypoint] Resolver auto-detectado: $NGINX_RESOLVER"
+
+# Sustituye variables del template
+envsubst < /usr/local/openresty/nginx/conf/nginx.conf.template > /usr/local/openresty/nginx/conf/nginx.conf
 
 exec /usr/local/openresty/bin/openresty -g "daemon off;"
