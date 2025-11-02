238c037dd08bf9bf00a5e4bd9bed91e87b00e4c1
Niji <mathereall@gmail.com>
2025-11-02 16:31:19 -0600

    fix: update nginx.conf handling in entrypoint script for resolver substitution

--- nginx-conf/entrypoint.sh
+++ nginx-conf/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 RESOLVER=$(awk '/^nameserver/{print $2; exit}' /etc/resolv.conf)
-export NGINX_RESOLVER=$RESOLVER
-echo "[entrypoint] Resolver auto-detectado: $NGINX_RESOLVER"
+echo "[entrypoint] Resolver auto-detectado: $RESOLVER"
 
-# Sustituye variables del template
-envsubst < /usr/local/openresty/nginx/conf/nginx.conf.template > /usr/local/openresty/nginx/conf/nginx.conf
+# Sustituye la variable en nginx.conf
+sed -i "s|\$NGINX_RESOLVER|$RESOLVER|g" /usr/local/openresty/nginx/conf/nginx.conf
 
 exec /usr/local/openresty/bin/openresty -g "daemon off;"
